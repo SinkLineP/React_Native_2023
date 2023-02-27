dde03ebdc9b405c00ea8824af874b289077ef633
SinkLineP <popovartem505@gmail.com>
2023-02-27 13:38:53 +0300

    finish lesson, create form

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function App() {
   const [name, setName] = useState("testName");
@@ -11,24 +11,49 @@ export default function App() {
     return <Text style={style}>{variable}</Text>
   }
 
-
   return (
     <View style={styles.container}>
-      <Text>My name: {showTextWithVariables(name, styles.text)}, i`m here <Text>{showTextWithVariables(age, styles.text)}</Text></Text>
+      <Text>Enter your name: </Text>
+      <TextInput
+        style={styles.input}
+        placeholder={"Enter your name.."}
+        onChangeText={(value) => setName(value)}
+      />
+      <Text>Enter your age: </Text>
+      <TextInput
+        keyboardType={"numeric"}
+        style={styles.input}
+        placeholder={"Enter your age.."}
+        onChangeText={(value) => setAge(Number(value))}
+      />
+      <Text>
+        ============================================{"\n"}
+        My name: {showTextWithVariables(name, styles.text)}.{"\n"}
+        I`m: {showTextWithVariables(age === 0 ? "test age" : age, styles.text)}.{"\n"}
+        ============================================
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    text: {
-      color: "red",
-      fontWeight: "bold"
-    }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: "red",
+    fontWeight: "bold"
+  },
+  input: {
+    borderColor: "#1e9dff",
+    borderWidth: 1,
+    padding: 8,
+    // margin: 10,
+    width: 200,
+    borderRadius: 4
+  }
 });
