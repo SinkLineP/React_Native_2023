9523c0ff1f965d58c806703b30007c1fd688e9c1
SinkLineP <popovartem505@gmail.com>
2023-02-27 16:14:26 +0300

    finish lesson 8

--- App.js
+++ App.js
@@ -1,31 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import {StyleSheet, Text, View, TouchableOpacity, FlatList} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity, FlatList, TextInput, Button} from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
-    {name: "mario1", id: 1},
-    {name: "mario2", id: 2},
-    {name: "mario3", id: 3},
-    {name: "mario4", id: 4},
-    {name: "mario5", id: 5},
-    {name: "mario6", id: 6},
-    {name: "mario7", id: 7},
-    {name: "mario8", id: 8},
-  ])
+    {name: "Миша", id: 1},
+    {name: "Карина", id: 2},
+    {name: "Артем", id: 3},
+    {name: "Мама", id: 4},
+    {name: "Папа", id: 5},
+    {name: "Бабушка", id: 6},
+    {name: "Дедушка", id: 7},
+    {name: "Кот", id: 8},
+  ]);
+
+  const pressHandler = (id) => {
+    setPeople((prevPeople) => {
+      return people.filter(person => person.id !== id)
+    })
+  }
+
+  const addNewPeople = (arrayPeoples) => {
+    const [userName, setUserName] = useState("");
+    let key = arrayPeoples.length === 0 ? 0 : arrayPeoples.length;
+
+    const handleChange = (val) => {
+      setPeople({name: val, key: key})
+      setUserName(val)
+    }
+
+    return(
+      <View style={styles.ContainerAddUser}>
+        <TextInput value={userName} onChangeText={(val) => handleChange(val)} placeholder={"Введите имя сотрудника.."} />
+      </View>
+    )
+  }
+
+  const [isShow, setIsShow] = useState(false);
 
   return (
     <View style={styles.container}>
+      {people.length === 0 ? (
+        <>
+          <Text>Больше некого увольнять..</Text>
+          <Text>Заходи позже)</Text>
+          <Text>Хотите добавить новых сотрудников?</Text>
+          <Button title={"Добавить"} onPress={() => setIsShow(true)} />
 
-      <FlatList
-        numColumns={2}
-        keyExtractor={(item) => item.id}
-        data={people}
-        renderItem={({ item }) => (
-          <Text style={styles.item}>{item.name}</Text>
-        )}
-      />
-
+          {isShow ? (
+            <Text>hi</Text>
+          ) : (
+            addNewPeople(people)
+          )}
+        </>
+      ) : (
+        <>
+          <Text>Выбери, кого хотел бы уволить)</Text>
+          <FlatList
+            numColumns={2}
+            keyExtractor={(item) => item.id}
+            data={people}
+            renderItem={({ item }) => (
+              <TouchableOpacity onPress={() => pressHandler(item.id)}>
+                <Text style={styles.item}>{item.name}</Text>
+              </TouchableOpacity>
+            )}
+          />
+        </>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -44,6 +86,11 @@ export const styles = StyleSheet.create({
     backgroundColor: "pink",
     fontSize: 24,
     marginHorizontal: 10,
+  },
+  ContainerAddUser: {
+    backgroundColor: "gray",
+    padding: 10,
+    margin: 10
   }
 });
 
