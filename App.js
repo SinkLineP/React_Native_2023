0d10a1c9809b12b9976ed52093fe60e2ac5f8f61
SinkLineP <popovartem505@gmail.com>
2023-02-27 11:47:36 +0300

    ...init...

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState("testName");
+  const [age, setAge] = useState(0);
+
+
+  const showTextWithVariables = (variable, style) => {
+    return <Text style={style}>{variable}</Text>
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name: {showTextWithVariables(name, styles.text)}, i`m here <Text>{showTextWithVariables(age, styles.text)}</Text></Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    text: {
+      color: "red",
+      fontWeight: "bold"
+    }
 });
